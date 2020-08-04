92580edbd63e77a92b53c068842cc1b5213e55e4
Sergey Xursevich <hursik94@gmail.com>
2020-08-04 21:49:45 +0300

    add: resolve tasks

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,1 +1,48 @@
-export { default } from './Chat';
+import React, { Component } from "react";
+import Message from "../Message";
+import "./Chat.css";
+
+export default class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      messageInput: '',
+    }
+  }
+
+  changeInputMessage = (e) => {
+    this.setState({messageInput: e.target.value});
+  };
+
+  sendMessageOnEnter = (e) => {
+    if (e.keyCode === 13) {
+      this.setState(({messages, messageInput}) => ({
+        messages: [...messages, { text: messageInput }],
+        messageInput: '',
+      }))
+    }
+  };
+
+  render() {
+    const { messages } = this.state;
+    return (
+      <div className="chat">
+        <div className="message-list">
+          <div className="messages">
+            {messages.map((item, index) => (
+              <Message key={`${item.text}+${index}`} text={item.text} />
+            ))}
+          </div>
+        </div>        
+        <input 
+          type="text" 
+          className="input.input-message" 
+          value={this.state.messageInput} 
+          onChange={this.changeInputMessage}
+          onKeyUp={this.sendMessageOnEnter}
+        />
+      </div>
+    )
+  }
+}
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,1 +1,15 @@
-export { default } from './Message';
+import React, { Component } from "react";
+import "./Message.css";
+
+export default class Message extends Component {
+  constructor(props) {
+    super(props);
+  } 
+
+  render() {
+    const { text } = this.props;
+    return (
+      <span className="message">{text}</span>
+    );
+  };
+}
